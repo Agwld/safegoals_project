fea39e7781dffa3ea1e1c0b202f301fd77480e28
sebastianmiyake-bit <sebastianmiyake@btinternet.com>
2026-02-20 17:51:18 +0000

    Add files via upload

--- hub_ilmatto/src/hub.c
+++ hub_ilmatto/src/hub.c
@@ -1,36 +1,54 @@
 #include "hub_buttons.h"
+#include "gpio.h"
+#include "comms.h"
 
+Message store[MAX_SEATS] ={0};
+
+Message* update_store(void){
+	uint16_t i;
+	for (i=0;i<MAX_SEATS;i++){
+			store[i] = check_seat(i);
+			update_displays_from_message(store[i],i);
+	}
+	return store;
+}
 
 int main(){
 	DDRA=0x00;
 	PORTA=0x00;
-	
+		
 	init_debug_uart0();
+	mcp23017_init();
+	init_i2c_master();
+	
 	uint8_t team;
 	while(1){
-		_delay_ms(100);
-		if (PINA & _BV(PINA0)){
+		//get seat data
+		update_store();
+		
+		//hub commands
+		if (PINA & _BV(PINA0)){//emergency signal on latch button
 			start_emerg();
-			while(1){
-				printf("1");
-				if(PINA & _BV(PINA4)){
-					printf("\nstopped \n");
-					break;
-				}
-			}
 		}
-		else if (PINA & _BV(PINA1)){//team A
+		else if (PINA & _BV(PINA1)){//team A goal
 			team = 1;
 			start_goal(team);
 		}
-		else if (PINA & _BV(PINA2)){//team B
+		else if (PINA & _BV(PINA2)){//team B goal
 			team = 0;
 			start_goal(team);
 		}
-		else if (PINA & _BV(PINA3)){
+		else if (PINA & _BV(PINA3)){//tug of war minigame
 			start_tow();
-			process_tow();
-			printf("A: %d  B:  %d\n", tot_a_count, tot_b_count);
+			uint8_t timer = 100;
+			while(timer > 0){
+				update_store();//gain data from seat to process
+				process_tow(store);
+				timer--;
+			}
+		}
+		else{//idle
+			idle();
 		}
 	}
 }
